1923d39907687f22e56bbc7f0dac82b4d569e09a
William Palmer <William Palmer>
2016-02-07 22:47:46 +0000

    real reformat

---
+++ Week 14/ZOO/src/Chocolate.java
@@ -0,0 +1,14 @@
+public class Chocolate extends Food {
+
+
+    public String eaten(Animal animal) {
+        return "animal eats chocolate";
+    }
+
+    public String eaten(Dog dog) {
+        return "dog eats chocolate";
+    }
+    public String eaten(Cat cat) {
+        return "cat eats chocolate";
+    }
+}
